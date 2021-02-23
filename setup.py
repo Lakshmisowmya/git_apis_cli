e5498cd76d4e3b0931f695bf9ccc30b4e01e338b
LakshmiSowmya <l.gunupudi@f5.com>
2021-02-23 22:56:41 +0530

    CLI for github apis

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='git-apis',  
+     version='0.2',
+     scripts=['git_apis'] ,
+     author="LakshmiSowmyaG",
+     author_email="sowmyalakshmi35@gmail.com",
+     description="A GitHub api utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/Lakshmisowmya/git_apis_cli",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
